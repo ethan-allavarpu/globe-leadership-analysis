731d5c0aff193fafb1070176e4dfd6fb1a19b284
Ethan Allavarpu <ethanallavarpu@g.ucla.edu>
2021-11-07 15:30:28 -0800

    preliminary autocratic data analysis

--- ethan_eda.R
+++ ethan_eda.R
@@ -7,9 +7,7 @@ society <- read.csv("GLOBE-Phase-2-Aggregated-Societal-Culture-Data.csv")
 society$Country.Name[society$Country.Name == "IRAN"] <- "Iran"
 
 combined_data <- full_join(leadership, society,
-                           by = c("Country", "Country.Name"))
-
-View(combined_data)
+                           by = c("Country", "Country.Name", "Country.Cluster"))
 
 countries <- data.frame("country" = leadership$Country.Name)
 iso_codes <- as.data.frame(iso_codes)
@@ -17,14 +15,41 @@ names(iso_codes) <- c("code", "country")
 country_codes <- countries %>% left_join(iso_codes)
 country_codes[is.na(country_codes$code), "code"] <- c("GBR", "CZE", "VEN",
                                                       "IRN", "RUS", "KOR",
-                                                      "ZAF", "BOL", NA, "ZAF",
-                                                      "CAN", "GER", "GER", "USA")
+                                                      "ZAF", "BOL", NA,
+                                                      "ZAF", "CAN", "GER",
+                                                      "GER", "USA")
 
 
 combined_data <- combined_data %>% mutate("Code" = country_codes$code)
-
 mapped_data <- joinCountryData2Map(combined_data, nameJoinColumn = "Code")
 
-mapCountryData(mapped_data, nameColumnToPlot = "Autocratic")
+par(mai = rep(0.85, 4), xaxs = "i", yaxs = "i")
+
+
+# Edit the following function to create maps for different variables
+# nameColumnToPlot should be the variable name
+# numCats defines how many breaks you want (44 is the maximum)
+# colourPalette defines the range of colors to plot
+## alpha in rgb() is the transparency factor
+### Edit the from and to arguments to match the variable of interest
+### Divide by the maximum value of the variable of interest
+
+mapCountryData(mapped_data, nameColumnToPlot = "Autocratic",
+               numCats = 44,
+               colourPalette = rgb(0.5, 0, 0,
+                                   alpha = seq(from = 1.89, to = 3.86,
+                                               length.out = 44) / 3.86),
+               borderCol = "black",
+               missingCountryCol = rgb(0, 0, 0, alpha = 0.5),
+               mapTitle = "Autocratic Levels by Country")
+
+combined_data %>%
+  group_by(Country.Cluster) %>%
+  summarize(avg_autocratic = mean(Autocratic),
+            med_autocratic = median(Autocratic)) %>%
+  arrange(desc(avg_autocratic))
+
+boxplot(Autocratic ~ Country.Cluster, data = combined_data,
+        cex.axis = 0.5, las = 2)
 
 
