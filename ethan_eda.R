fa107eafc3feaf4b462112b3713051f544d6a54d
Ethan Allavarpu <ethanallavarpu@g.ucla.edu>
2021-11-07 13:37:49 -0800

    updated heatmap code

--- ethan_eda.R
+++ ethan_eda.R
@@ -1,18 +1,30 @@
 library(tidyverse)
 library(rworldmap)
-source("iso_codes")
+source("iso_codes.R")
 
 leadership <- read.csv("GLOBE-Phase-2-Aggregated-Leadership-Data.csv")
 society <- read.csv("GLOBE-Phase-2-Aggregated-Societal-Culture-Data.csv")
+society$Country.Name[society$Country.Name == "IRAN"] <- "Iran"
 
 combined_data <- full_join(leadership, society,
                            by = c("Country", "Country.Name"))
 
 View(combined_data)
 
-autocratic <- leadership %>% select(Country, Country.Name, Autocratic)
+countries <- data.frame("country" = leadership$Country.Name)
+iso_codes <- as.data.frame(iso_codes)
+names(iso_codes) <- c("code", "country")
+country_codes <- countries %>% left_join(iso_codes)
+country_codes[is.na(country_codes$code), "code"] <- c("GBR", "CZE", "VEN",
+                                                      "IRN", "RUS", "KOR",
+                                                      "ZAF", "BOL", NA, "ZAF",
+                                                      "CAN", "GER", "GER", "USA")
 
-mapped_data <- joinCountryData2Map(autocratic, joinCode = "NAME", nameJoinColumn = "Country")
+
+combined_data <- combined_data %>% mutate("Code" = country_codes$code)
+
+mapped_data <- joinCountryData2Map(combined_data, nameJoinColumn = "Code")
 
 mapCountryData(mapped_data, nameColumnToPlot = "Autocratic")
-heatmap_world(autocratic)
+
+
